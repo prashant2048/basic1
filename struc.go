020c9233b090ad6157e717efc3ea54bc4c328d88
prashant2048 <51480353+prashant2048@users.noreply.github.com>
2019-06-07 17:06:50 +0530

    upload

---
+++ struc.go
@@ -0,0 +1,17 @@
+package main
+import ("fmt" )
+type st struct {
+x int 
+y int
+
+
+}
+func main(){
+	var z int
+	fmt.Scanln(&z)
+	s:=st{1,2}
+	fmt.Println(s)
+	fmt.Println(z)
+	
+	//fmt.Println("%T",z)
+}
