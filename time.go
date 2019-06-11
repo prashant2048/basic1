093ff1b44e42613310d635a3571d855821866749
prashant2048 <51480353+prashant2048@users.noreply.github.com>
2019-06-11 12:03:37 +0530

    Add files via upload

---
+++ time.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt" 
+"net"
+"time"
+)
+func main() {	
+	t:=time.Now()
+	fmt.Println(t.Location())
+   	ipr,_:=net.LookupIp("facebook.com")
+	for _, ip:=range ipr {
+		fmt.Println(ip)
+	}
+}
